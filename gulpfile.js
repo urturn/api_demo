61a7497d0dc353e4313399220308835dcf0d9c74
Pierre-Loic Doulcet <pierre.doulcet@webdoc.com>
2014-02-13 16:03:28 +0100

    host dependent script

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,13 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var closureCompiler = require('gulp-closure-compiler');
 var concat = require('gulp-concat');
+var replace = require('gulp-replace');
 
 gulp.task('build', function(){
  return gulp.src(['./urturn-api.js', './urturn-expression-widget.js'])
   .pipe(closureCompiler())
-  .pipe(concat('urturn-expression-widget.min.js'))
+  .pipe(replace('www.urturn.com', "<%=  APP_CONFIG['host'] %>"))
+  .pipe(concat('urturn-expression-widget.min.js.erb'))
   .pipe(gulp.dest('./build'));
 });
 
